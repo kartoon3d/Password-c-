5130802e2cddb4af9a26bf951c3bce932cc5fba8
VG Rig <VG Rig@DESKTOP-K8OKD1L>
2021-09-08 11:27:26 +0200

    Aggiungere i file di progetto.

---
+++ test3/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace test3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTipologia = new System.Windows.Forms.ComboBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NomeSito = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipologia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edita = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Cancella = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.txtRicerca = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sito Web";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNote);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtTipologia);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(37, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 227);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inserimento Dati";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(106, 188);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(193, 20);
+            this.txtNote.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Note";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tipologia";
+            // 
+            // txtTipologia
+            // 
+            this.txtTipologia.FormattingEnabled = true;
+            this.txtTipologia.Items.AddRange(new object[] {
+            "Cpanel",
+            "Ftp",
+            "Wordpress",
+            "Accesso admin"});
+            this.txtTipologia.Location = new System.Drawing.Point(106, 84);
+            this.txtTipologia.Name = "txtTipologia";
+            this.txtTipologia.Size = new System.Drawing.Size(193, 21);
+            this.txtTipologia.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(106, 162);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(193, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(106, 135);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(193, 20);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(106, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(193, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Salva";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Pulisci";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.NomeSito,
+            this.Tipologia,
+            this.Edita,
+            this.Cancella});
+            this.dataGridView1.Location = new System.Drawing.Point(400, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(537, 450);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // NomeSito
+            // 
+            this.NomeSito.HeaderText = "Nome Sito";
+            this.NomeSito.Name = "NomeSito";
+            // 
+            // Tipologia
+            // 
+            this.Tipologia.HeaderText = "Tipologia";
+            this.Tipologia.Name = "Tipologia";
+            // 
+            // Edita
+            // 
+            this.Edita.HeaderText = "Edita";
+            this.Edita.Name = "Edita";
+            this.Edita.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Edita.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Edita.Text = "Edita";
+            this.Edita.UseColumnTextForButtonValue = true;
+            // 
+            // Cancella
+            // 
+            this.Cancella.HeaderText = "Cancella";
+            this.Cancella.Name = "Cancella";
+            this.Cancella.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Cancella.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Cancella.Text = "Cancella";
+            this.Cancella.UseColumnTextForButtonValue = true;
+            // 
+            // txtRicerca
+            // 
+            this.txtRicerca.Location = new System.Drawing.Point(51, 419);
+            this.txtRicerca.Name = "txtRicerca";
+            this.txtRicerca.Size = new System.Drawing.Size(167, 20);
+            this.txtRicerca.TabIndex = 9;
+            this.txtRicerca.TextChanged += new System.EventHandler(this.txtRicerca_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ricerca";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 526);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtRicerca);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox txtTipologia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NomeSito;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipologia;
+        private System.Windows.Forms.DataGridViewButtonColumn Edita;
+        private System.Windows.Forms.DataGridViewButtonColumn Cancella;
+        private System.Windows.Forms.TextBox txtRicerca;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ test3/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+
+        MySqlConnection con = new MySqlConnection("datasource=127.0.0.1;port=3306;database=test;username=root;password=;SslMode=none");
+        MySqlCommand cmd;
+        MySqlDataReader read;
+
+
+
+        string sql;
+        bool Mode = true;
+
+        public void LoadGrid()
+
+        {
+            try 
+            {
+                sql = "SELECT * FROM siti_web";
+                cmd = new MySqlCommand(sql, con);
+                con.Open();
+
+                dataGridView1.Rows.Clear();
+                read = cmd.ExecuteReader();
+                while (read.Read()) 
+                {
+                    dataGridView1.Rows.Add(read[0], read[1], read[2], read[3]);
+                }
+                con.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+        }
+
+        public void getId(String id)
+        {
+            sql = "SELECT * FROM siti_web where id = '" + id +  "'   ";
+
+            cmd = new MySqlCommand(sql,con);
+            con.Open();
+            read = cmd.ExecuteReader();
+            while(read.Read())
+            {
+                txtName.Text = read[1].ToString();
+                txtTipologia.Text = read[2].ToString();
+                txtUsername.Text = read[3].ToString();
+                txtNote.Text = read[5].ToString();
+                txtPassword.Text = Base64Decode(read[4].ToString());
+
+            }
+            con.Close();
+        
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtUsername.Clear();
+            txtPassword.Clear();
+            txtNote.Clear();
+            txtRicerca.Clear();
+            LoadGrid();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string tipologia = txtTipologia.Text;
+            string username = txtUsername.Text;
+            string password = Base64Encode(txtPassword.Text);
+          
+            string id;
+            string note = txtNote.Text;
+
+            if (Mode == true)
+            {
+                sql = "INSERT INTO siti_web(NomeSito,Tipologia,Username,Password,Note) values(@name,@tipologia,@username,@password,@note)";
+                con.Open();
+                cmd = new MySqlCommand(sql,con);
+                cmd.Parameters.AddWithValue("@name",name);
+                cmd.Parameters.AddWithValue("@tipologia",tipologia);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@note", note);
+                MessageBox.Show("inserito");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtUsername.Clear();
+                txtPassword.Clear();
+                txtNote.Clear();
+                con.Close();
+                LoadGrid();
+
+            }
+            else 
+            {
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "UPDATE siti_web SET NomeSito=@name, Tipologia=@tipologia, Username=@username, Password=@password, Note=@note WHERE id=@id";
+                con.Open();
+                cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@tipologia", tipologia);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@note", note);
+                cmd.Parameters.AddWithValue("@id", id);
+                MessageBox.Show("aggiornato");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtUsername.Clear();
+                txtPassword.Clear();
+                txtNote.Clear();
+                Mode = true;
+                con.Close();
+                LoadGrid();
+            }
+           
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id;
+            if (e.ColumnIndex == dataGridView1.Columns["Edita"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString() ;
+                getId(id);
+            }
+            else if (e.ColumnIndex == dataGridView1.Columns["Cancella"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "DELETE FROM siti_web WHERE id=@id";
+                con.Open();
+                cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@id", id);
+                MessageBox.Show("cancellato");
+                cmd.ExecuteNonQuery();
+                con.Close();
+                LoadGrid();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRicerca_TextChanged(object sender, EventArgs e)
+        {
+            SearchData(txtRicerca.Text);
+        }
+
+        public void SearchData(string search)
+        {
+            string sql = "SELECT * FROM siti_web WHERE NomeSito like '%" + search + "%' ";
+            con.Open();
+            cmd = new MySqlCommand(sql, con);
+
+            dataGridView1.Rows.Clear();
+            read = cmd.ExecuteReader();
+
+            while (read.Read())
+            {
+                dataGridView1.Rows.Add(read[0], read[1], read[2], read[3]);
+            }
+
+            
+            con.Close();
+        }
+    }
+}
